b195f5a968ef6fe641fc3c6e6da2a990d292644b
Nikhil Jha <hi@nikhiljha.com>
2018-10-08 08:40:16 -0700

    still fail on errors

--- ethyl/run.sh
+++ ethyl/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# set -e
+set -e
 
 CONFIG_PATH=/data/options.json
 
 # PLAIN=$(jq --raw-output ".plain" $CONFIG_PATH)
 
-exec mkdir ~/.config
-exec mkdir ~/.config/configstore
-exec cp $CONFIG_PATH ~/.config/configstore/ethyl-hass.json
+mkdir ~/.config || true
+mkdir ~/.config/configstore || true
+cp $CONFIG_PATH ~/.config/configstore/ethyl-hass.json || true
 
 # start server
 exec node /etc/ethyl-hass/index.js < /dev/null
